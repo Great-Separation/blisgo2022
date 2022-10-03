6646dcb5fc070ea8dd279cd067058de08293bc59
Ok Jae Ook <okjaeook98@gmail.com>
2022-10-04 02:20:04 +0900

    Mapsturct 오타 수정

--- src/main/java/com/blisgo/domain/mapper/GuideMapper.java
+++ src/main/java/com/blisgo/domain/mapper/GuideMapper.java
@@ -1,13 +1,13 @@
 package com.blisgo.domain.mapper;
 
-import com.blisgo.domain.entity.Dogam;
+import com.blisgo.domain.entity.Guide;
 import com.blisgo.domain.mapper.cmmn.GenericMapper;
-import com.blisgo.web.dto.DogamDTO;
+import com.blisgo.web.dto.GuideDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface GuideMapper extends GenericMapper<DogamDTO, Dogam> {
+public interface GuideMapper extends GenericMapper<GuideDTO, Guide> {
     GuideMapper INSTANCE = Mappers.getMapper(GuideMapper.class);
 }

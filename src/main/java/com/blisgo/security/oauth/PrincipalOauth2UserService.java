00909b8ecefc6e68a185657ea9c4f7d78846dbbe
Ok Jae Ook <okjaeook98@gmail.com>
2022-11-01 07:33:24 +0900

    OAuth 호출 값 수정 DefaultOAuth2UserService가 배포환경 JDK에 들어있지 않음 구현체 OAuth2UserService<OAuth2UserRequest,OAuth2User> 그대로 사용함

--- src/main/java/com/blisgo/security/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/blisgo/security/oauth/PrincipalOauth2UserService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -23,14 +24,18 @@ import java.util.Optional;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+public class PrincipalOauth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final AccountRepository accountRepository;
     private final HttpSession httpSession;
 
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
+        //OAuth2UserService<OAuth2UserRequest, OAuth2User>
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        //Class DefaultOAuth2UserService
+        //OAuth2User oAuth2User = super.loadUser(userRequest);
 
         log.info("userRequest clientRegistration : " + userRequest.getClientRegistration());
         log.info("oAuth2User : " + oAuth2User);

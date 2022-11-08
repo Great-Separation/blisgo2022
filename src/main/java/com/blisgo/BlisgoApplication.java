c2ccfa3bd6b581374f81b8110cf9a13c6d765919
Ok Jae Ook <okjaeook98@gmail.com>
2022-11-08 21:59:31 +0900

    배경 변경 실행 위치 조정

--- src/main/java/com/blisgo/BlisgoApplication.java
+++ src/main/java/com/blisgo/BlisgoApplication.java
@@ -15,11 +15,11 @@ public class BlisgoApplication {
     public void started() {
         // 서버 시간 표준으로 고정(UTC+-0)
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-        // 서버 실행 시 index 배경 화면 변경. 1 client -> 1 서버 스레드 실행. 시간 당 50최대
-        Unsplash.changeWallpaper();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(BlisgoApplication.class, args);
+        // 서버 실행 시 index 배경 화면 변경. 1 client -> 1 서버 스레드 실행. 시간 당 50최대
+        Unsplash.changeWallpaper();
     }
 }

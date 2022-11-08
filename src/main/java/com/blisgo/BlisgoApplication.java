09c6d0f26cbd63fdfd91f2cc0538dfb1e41c4c85
Ok Jae Ook <okjaeook98@gmail.com>
2022-11-08 21:41:30 +0900

    index 배경화면 변경 로직 수정

--- src/main/java/com/blisgo/BlisgoApplication.java
+++ src/main/java/com/blisgo/BlisgoApplication.java
@@ -1,5 +1,6 @@
 package com.blisgo;
 
+import com.blisgo.util.Unsplash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +15,8 @@ public class BlisgoApplication {
     public void started() {
         // 서버 시간 표준으로 고정(UTC+-0)
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        // 서버 실행 시 index 배경 화면 변경. 1 client -> 1 서버 스레드 실행. 시간 당 50최대
+        Unsplash.changeWallpaper();
     }
 
     public static void main(String[] args) {
